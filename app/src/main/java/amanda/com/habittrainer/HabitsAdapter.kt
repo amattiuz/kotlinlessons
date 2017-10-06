1edc9c430344fe611dedd9b0514dd632eac78e19
amattiuz <amattiuz@motorola.com>
2017-10-06 10:19:49 -0300

    Adding menu

--- app/src/main/java/amanda/com/habittrainer/HabitsAdapter.kt
+++ app/src/main/java/amanda/com/habittrainer/HabitsAdapter.kt
@@ -1,26 +1,39 @@
 package amanda.com.habittrainer
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.single_card.view.*
 
 /**
  * Created by venturus on 03/10/17.
  */
 
 class HabitsAdapter(val habits: List<Habit>) : RecyclerView.Adapter<HabitsAdapter.HabitViewHolder>(){
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): HabitViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+    class HabitViewHolder(val card: View) : RecyclerView.ViewHolder(card)
+
+    //one card == one item
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.single_card, parent, false)
+        return HabitViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: HabitViewHolder?, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if(holder != null)
+        {
+            val habit = habits[position]
+            holder.card.tv_title.text = habit.title
+            holder.card.tv_description.text = habit.description
+            holder.card.iv_icon.setImageResource(habit.image)
+        }
     }
 
 
     override fun getItemCount(): Int = habits.size
 
-    class HabitViewHolder(val iv: View) : RecyclerView.ViewHolder(iv)
+
 
 
 }
